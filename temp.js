4d9e8eb8d784a2de765c0d582c5bba4f64f8c138
maxmiliano <maxmilianossouza@hotmail.com>
2023-04-18 23:16:55 -0300

    construindo js

--- temp.js
+++ temp.js
@@ -1,6 +1,15 @@
-const chave = "b593b4b337384bb9a3899a9fce9d739"
+const chave = "b593b4b337384bb9a3899a9fce9d739a"
+
+async function buscarCidade(cidade) { /* funcao ASYNC para buscar do servidor*/
+
+     const dadosServidor = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chave}`). then(resposta => resposta.json())
+      /* await para esperar o servidor carregar*/
+    console.log(dadosServidor)
+}
+
 
 function clickBt() {
-    const input = document.querySelector(".place").value 
-     console.log(input)
+    const cidade = document.querySelector(".place").value 
+
+    buscarCidade(cidade)
 }
