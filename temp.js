61ec98db32ec70e6495e9b8e9285ec01f46b3046
maxmiliano <maxmilianossouza@hotmail.com>
2023-04-19 23:19:07 -0300

    finalizado js

--- temp.js
+++ temp.js
@@ -1,16 +1,15 @@
 const chave = "b593b4b337384bb9a3899a9fce9d739a"
 
 function telaUsuario(dadosServidor) {
-     /*console.log(dadosServidor)*/
+     /*console.log(dadosServidor) usado para testar a aplicação*/
      console.log(dadosServidor)
      document.querySelector(".cidad").innerHTML = " Tempo em " + dadosServidor.name
      document.querySelector(".grau").innerHTML = Math.floor(dadosServidor.main.temp) + "°C"
      document.querySelector(".previsao").innerHTML = dadosServidor.weather[0].description
-     document.querySelector(".humid").innerHTML = dadosServidor.main.humidity
+     document.querySelector(".humid").innerHTML = dadosServidor.main.humidity + "%"
+     document.querySelector(".imgtempo").src = `https://openweathermap.org/img/wn/${dadosServidor.weather[0].icon}.png`
 
-     
-
-
+     /* o dados reais do tempo sao tirados do site https://openweathermap.org/*/
 }
 
 async function buscarCidade(cidade) { /* funcao ASYNC para buscar do servidor*/
@@ -20,9 +19,7 @@ async function buscarCidade(cidade) { /* funcao ASYNC para buscar do servidor*/
 
       telaUsuario(dadosServidor)
 
-    
 }
-
 
 function clickBt() {
     const cidade = document.querySelector(".place").value 
